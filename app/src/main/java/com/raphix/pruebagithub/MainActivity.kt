04cdb04ce0a865bff9a00ff0f9e8a89c87bf8867
raphix <raphix@gmail.com>
2023-10-03 19:20:19 +0200

    Initial commit

---
+++ app/src/main/java/com/raphix/pruebagithub/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.raphix.pruebagithub
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.raphix.pruebagithub.R.*
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+
+        var boton = findViewById<Button>(R.id.boton)
+        var texto = findViewById<TextView>(R.id.texto)
+
+        boton.setOnClickListener {
+            texto.text = "GILIPOLLAS"
+        }
+    }
+}
